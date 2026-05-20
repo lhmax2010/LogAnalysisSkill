39fa52bd4dfc5730d45f4f5991c5ec0200667371
lhmax2010 <178032326@qq.com>
2026-05-20 14:34:17 +0800

    feat(workflow): register bw m3 suggesters

--- gbs_workflow/suggesters/registry.py
+++ gbs_workflow/suggesters/registry.py
@@ -3,6 +3,20 @@
 from __future__ import annotations
 
 from gbs_workflow.suggesters.base import SuggesterBase
+from gbs_workflow.suggesters.compile_error import CompileErrorSuggester
 from gbs_workflow.suggesters.depsolve import DepsolveSuggester
+from gbs_workflow.suggesters.fallback import FallbackSuggester
+from gbs_workflow.suggesters.linker_missing import LinkerMissingSuggester
+from gbs_workflow.suggesters.linker_undef import LinkerUndefSuggester
+from gbs_workflow.suggesters.patch_failed import PatchFailedSuggester
+from gbs_workflow.suggesters.spec_script import SpecScriptSuggester
 
-DEFAULT_SUGGESTERS: list[SuggesterBase] = [DepsolveSuggester()]
+DEFAULT_SUGGESTERS: list[SuggesterBase] = [
+    DepsolveSuggester(),
+    LinkerMissingSuggester(),
+    LinkerUndefSuggester(),
+    PatchFailedSuggester(),
+    SpecScriptSuggester(),
+    CompileErrorSuggester(),
+    FallbackSuggester(),
+]
